e70ffcf135cb63725309b4d0e6c4af309272a807
mkorniie <korniiets.maria.mail@gmail.com>
2019-08-19 14:17:24 +0300

    Init commit - Basic Controller and index page; not functional

---
+++ src/main/java/ua/mkorniie/manager/ManagerApplication.java
@@ -0,0 +1,13 @@
+package ua.mkorniie.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagerApplication.class, args);
+	}
+
+}
---
+++ src/main/java/ua/mkorniie/manager/controller/MainController.java
@@ -0,0 +1,15 @@
+package ua.mkorniie.manager.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String getMainPage() {
+        return "index";
+    }
+
+}
